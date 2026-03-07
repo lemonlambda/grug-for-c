20e9b3892f744e09d38f6f7170009c53d556d7cc
NikhilNathanael <nikhilnathanael2020@gmail.com>
2026-03-07 17:02:41 +0530

    updates from grug-rs example

--- src/grug.h
+++ src/grug.h
@@ -11,10 +11,17 @@ extern "C" {
 typedef uint64_t grug_id;
 
 typedef grug_id grug_on_fn_id;
+#define INVALID_GRUG_ON_FN_ID UINT64_MAX
 
 typedef grug_id grug_file_id;
+#define INVALID_GRUG_FILE_ID UINT64_MAX
 
-typedef grug_id grug_entity_id;
+// note: This should maybe be implementation specific
+// I wanna keep this a pointer so that users can get the me_id and file_id from
+// the entity directly
+// But we can also have an api where the user gets this pointer using the id
+typedef struct grug_entity* grug_entity_id;
+/* typedef grug_id grug_entity_id; */
 
 union grug_value {
     double _number;
@@ -25,10 +32,23 @@ union grug_value {
 
 struct grug_state;
 
-typedef void (*game_fn_void)(struct grug_state* gst, grug_id me, const union grug_value[]);
-typedef void (*game_fn_void_argless)(struct grug_state* gst, grug_id me);
-typedef union grug_value (*game_fn_value)(struct grug_state* gst, grug_id me, const union grug_value[]);
-typedef union grug_value (*game_fn_value_argless)(struct grug_state* gst, grug_id me);
+// Information about an entity. 
+// These fields should be treated as readonly by the game
+// Backends can modify `data` when initialing or deinitializing data
+struct grug_entity {
+	grug_id me_id;
+	grug_file_id file_id;
+	void* data;
+};
+
+// TODO: There should only be a single kind of game function: game_fn_value. 
+// if there are no arguments, the arguments array should be empty
+// if there is no return value, the actual return value should just be ignored
+
+typedef void (*game_fn_void)(struct grug_state* gst, const union grug_value[]);
+typedef void (*game_fn_void_argless)(struct grug_state* gst);
+typedef union grug_value (*game_fn_value)(struct grug_state* gst, const union grug_value[]);
+typedef union grug_value (*game_fn_value_argless)(struct grug_state* gst);
 
 enum grug_error_type_enum {
     grug_error_type_stack_overflow = 0,
@@ -43,8 +63,21 @@ struct grug_updates_list {
     struct grug_file** updates;
 };
 
-// TODO: use strings or give the user the actual ids to the script + function?
-typedef void (*runtime_error_handler)(char const* reason, grug_error_type type, char const* on_fn_name, char const* on_fn_path);
+// TODO: use strings or give the user the file_id and on_fn_id?
+struct grug_runtime_error_handler {
+	void* data;
+	void (*drop_fn)(void*);
+	void (*handler_fn)(
+		void* data,
+		uint32_t err_kind,
+		char* reason,
+		size_t reason_len,
+		char* on_fn_name,
+		size_t on_fn_name_len,
+		char* script_path,
+		size_t script_path_len
+	);
+};
 
 struct grug_on_fn_entry {
     char const* entity_name;
@@ -189,6 +222,7 @@ struct grug_expr {
 			char* function_name;
 			struct grug_expr* args;
 			size_t args_count;
+			void* game_fn_ptr;
 		} call;
 		struct grug_expr* parenthesized;
 	} expr_data;
@@ -281,13 +315,64 @@ struct grug_ast {
 
 /* AST */
 
-typedef void (*grug_backend_vtable_drop)(void* obj);
-typedef grug_file_id (*grug_backend_vtable_compile_script)(void* obj, struct grug_ast* ast);
+// Free all resource owned by the backend
+typedef void (*grug_backend_vtable_drop)(void* backend_data);
+/// The AST of a typechecked grug file is provided to let the backend do
+/// further transforms and lower to bytecode or even machine code
+/// `ast` owns allocations that are freed once this function returns. Ensure
+/// all resources (including strings) are copied out before it returns;
+/// 
+/// The script ids are guaranteed to be in contiguous ascending order.
+///
+/// If the same script id is returned again, then it means the old script
+/// associated with the id should be destroyed and replaced with this one. 
+///
+/// The entity data of all entities created from the old script should be
+/// regenerated
+typedef void (*grug_backend_vtable_compile_script)(void* backend_data, grug_file_id file_id, struct grug_ast ast);
+/// Initialize the member data of the newly created entity. When this
+/// function is called, the member field of `entity` points to garbage and
+/// must not be deinitialized. The GrugScriptId to be used is obtained from
+/// the file_id member of `entity`. 
+///
+/// `entity` is pinned until it is deinitialized by a call to
+/// `destroy_entity_data` or `insert_file` with the same path as its
+/// current GrugScriptId. The reference must be stored as a raw pointer
+/// within self so that it can be used during `destroy_entity_data` to
+/// check for pointer equality. 
+/// It is safe to use that pointer as a &GrugEntity in the meantime.
+///
+/// Returns false if there was a runtime error during execution
+typedef bool (*grug_backend_vtable_init_entity)(void* backend_data, struct grug_state* gst, struct grug_entity* entity); 
+/// Deinitialize all the data associated with all entities. The pointers
+/// stored during `init_entity` must be used to get access to the entity data.
+/// The entities can only be accessed as a &GrugEntity even self is available with an exclusive reference
+typedef bool (*grug_backend_vtable_clear_entities)(void* backend_data); 
+/// Deinitialize the data associated with `entity`. 
+typedef void (*grug_backend_vtable_destroy_entity_data)(void* backend_data, struct grug_entity* entity);
+/// Run the on function at index `on_fn_index` of the script associated
+/// with `entity`.
+///
+/// # SAFETY: `values` must point to an array of GrugValues of at least as
+/// many elements as the number of arguments to the on_ function
+///
+/// If the number of arguments is 0, then `values` is allowed to be null
+typedef bool (*grug_backend_vtable_call_on_function_raw)(void* backend_data, struct grug_state* gst, struct grug_entity* entity, uint64_t on_fn_index, union grug_value* args); 
+/// Run the on function at index `on_fn_index` of the script associated
+/// with `entity`.
+///
+/// # Panics: The length of `values` must exactly match the number of
+/// expected arguments to the on_ function
+typedef bool (*grug_backend_vtable_call_on_function)(void* backend_data, struct grug_state* gst, struct grug_entity* entity, uint64_t on_fn_index, union grug_value* args, size_t args_len); 
 
 struct grug_backend_vtable {
-    grug_backend_vtable_drop drop;
     grug_backend_vtable_compile_script compile_script;
-    // TODO: finish backend vtable
+	grug_backend_vtable_init_entity init_entity;
+	grug_backend_vtable_clear_entities clear_entities;
+	grug_backend_vtable_destroy_entity_data entity_data;
+	grug_backend_vtable_call_on_function_raw call_on_function_raw;
+	grug_backend_vtable_call_on_function call_on_function;
+    grug_backend_vtable_drop drop;
 };
 
 struct grug_backend {
@@ -295,63 +380,92 @@ struct grug_backend {
     struct grug_backend_vtable* vtable;
 };
 
+// TODO: This should prolly be implementation specific
 struct grug_init_settings {
-    void* user_alloc_obj;
-    runtime_error_handler runtime_error_handler;
-
-    // When null, grug assumes "[cwd]/mods"
-    char const* mods_folder;
-
-    struct grug_backend backend;
+	// TODO: We probably want a way to define the mod_api as a string (at least for prototyping)
+	char* mod_api_path;
+	size_t mod_api_path_len;
+	char* mods_dir_path;
+	size_t mods_dir_path_len;
+	struct grug_runtime_error_handler runtime_error_handler;
+	struct grug_backend backend;
 };
 
 struct grug_init_settings grug_default_settings(void);
 
 struct grug_state* grug_init(struct grug_init_settings settings);
 
-void grug_swap_backend(struct grug_state* gst, struct grug_backend backend);
-void grug_set_fast_mode(struct grug_state* gst, bool fast);
+// returns true if registration is successful
+// returns false if not.
+//
+// Reasons for failure include but are not limited to 
+// 	- function was not defined in `mod_api.json`. 
+// 	- function has already been registered
+bool grug_register_game_fn_void_argless(struct grug_state* gst, char const* game_fn_name, game_fn_void_argless fn);
+bool grug_register_game_fn_value_argless(struct grug_state* gst, char const* game_fn_name, game_fn_value_argless fn);
+bool grug_register_game_fn_void(struct grug_state* gst, char const* game_fn_name, game_fn_void fn);
+bool grug_register_game_fn_value(struct grug_state* gst, char const* game_fn_name, game_fn_value fn);
 
-void grug_register_game_fn_void_argless(struct grug_state* gst, char const* game_fn_name, game_fn_void_argless fn);
-void grug_register_game_fn_value_argless(struct grug_state* gst, char const* game_fn_name, game_fn_value_argless fn);
-void grug_register_game_fn_void(struct grug_state* gst, char const* game_fn_name, game_fn_void fn);
-void grug_register_game_fn_value(struct grug_state* gst, char const* game_fn_name, game_fn_value fn);
+// Returns true if all game functions defined in mod_api.json are registered
+bool grug_all_game_functions_registered(struct grug_state* gst);
 
-/// Returns a list of all the fn ids for the mod_api.json
+// Get the on_fn_id for a particular on_ function for a particular entity
+grug_on_fn_id grug_get_on_fn_id(struct grug_state* gst, const char* entity_type, const char* on_fn_name);
+
+// Returns a list of all the fn ids for the mod_api.json
 struct grug_on_fns grug_get_fn_ids(struct grug_state* gst);
 
+// Compiles a single file from the mods directory
+grug_file_id grug_compile_file(struct grug_state* gst, const char* path);
+// Compile a file from a string. Useful for prototypeing or for built in scripts
+// If it overlaps with a path on the actual filesystem, it is given the same id as that path
+grug_file_id grug_compile_file_from_str(struct grug_state* gst, const char* path, char* file_text);
+
+// Compiles and inserts all grug files in the mods directory
 const struct grug_mod_dir* grug_get_mods(struct grug_state* gst);
 
-grug_entity_id grug_create_entity(struct grug_state* gst, grug_file_id script, grug_id id);
+// Instantiate an entity from a script
+grug_entity_id grug_create_entity(struct grug_state* gst, grug_file_id script);
 
-grug_file_id grug_entity_get_file(struct grug_state* gst, grug_entity_id entity);
+// Use this if `grug_entity_id` is not the same as `struct grug_entity*`
+grug_file_id grug_entity_get_file_id(struct grug_state* gst, grug_entity_id entity);
 
 // me_id
+// Use this if `grug_entity_id` is not the same as `struct grug_entity*`
 grug_id grug_entity_get_id(struct grug_state* gst, grug_entity_id entity);
 
+// Destroy the data associated with an entity. Using the same id after this
+// function is a UAF
 void grug_deinit_entity(struct grug_state* gst, grug_entity_id entity);
 
+// TODO: Resource management of this array?
 struct grug_updates_list grug_update(struct grug_state* gst);
 
+// Destroy a grug state and free all its resources
 void grug_deinit(struct grug_state* gst);
 
-void grug_backend_call_argless(struct grug_state* gst, grug_on_fn_id fn, grug_entity_id entity);
-void grug_backend_call(struct grug_state* gst, grug_on_fn_id fn, grug_entity_id entity, const union grug_value args[]);
+void grug_swap_backend(struct grug_state* gst, struct grug_backend backend);
+// TODO: Should this be done per script? or maybe per function call?
+void grug_set_fast_mode(struct grug_state* gst, bool fast);
 
-#define GRUG_CALL_ARGLESS(_state, _on_fn, _entity) grug_backend_call_argless(_state, _on_fn, _entity)
+// returns false if on function could not be executed or if there was a runtime error
+// `args` can be NULL if there are no arguments
+bool grug_call_on_function_raw(struct grug_state* gst, grug_entity_id entity, grug_on_fn_id on_fn_id, union grug_value* args);
+bool grug_call_on_function(struct grug_state* gst, grug_entity_id entity, grug_on_fn_id on_fn_id, union grug_value* args, size_t args_len);
 
-#define GRUG_CALL(_state, _on_fn, _entity, ...) \
-    do { \
-        const union grug_value _grug_args[] = {__VA_ARGS__}; \
-        grug_backend_call(_state, _on_fn, _entity, _grug_args); \
-    } while(0);
+#define GRUG_CALL_ARGLESS(_state, _entity, _on_fn_id) \
+        grug_call_on_function(_state, _entity, _on_fn_id, NULL, 0); \
+
+// TODO: This doesn't work with the safe version because the length is required
+#define GRUG_CALL(_state, _entity, _on_fn_id, _args_count, ...) \
+        grug_call_on_function(_state, _entity, _on_fn_id, (union grug_value[]) {__VA_ARGS__}, _args_count); \
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-function"
-static inline union grug_value GRUG_ARG_NUMBER(double v) { union grug_value r; r._number = v; return r; }
-static inline union grug_value GRUG_ARG_BOOL(bool v) { union grug_value r; r._bool = v; return r; }
-static inline union grug_value GRUG_ARG_STRING(char const* v) { union grug_value r; r._string = v; return r; }
-static inline union grug_value GRUG_ARG_ID(grug_id v) { union grug_value r; r._id = v; return r; }
+static inline union grug_value GRUG_ARG_NUMBER(double v) {return (union grug_value){._number = v};}
+static inline union grug_value GRUG_ARG_BOOL(bool v) {return (union grug_value){._bool = v};}
+static inline union grug_value GRUG_ARG_STRING(char const* v) {return (union grug_value){._string = v};}
+static inline union grug_value GRUG_ARG_ID(grug_id v) {return (union grug_value){._id = v};}
 #pragma GCC diagnostic pop
 
 // TODO: use a temporary allocator instead probably, might require a state though?
